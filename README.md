e3790e5c54edd1159f3692b8915621cd3a55f4f0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-02-18 14:28:38 +0100

    code cleanup, new configuration interface, krugs_config.py now loaded from ~/krugs_config.py

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Krugs - Simple daemon manager <img src="http://i.imgur.com/IJ6EHnq.png"/> <img src="http://i.imgur.com/59R7gD9.png"/>
+# Krugs - Simple daemon manager
+
+![license MIT](http://i.imgur.com/uc37iNC.png) ![status beta stable](http://i.imgur.com/4SVCaU2.png)
 
 Krugs is a simple manager for daemon processes. It is easier to use and
 deploy than using/writing `init.d` scripts and the Unix `service` command.
@@ -6,27 +8,84 @@ Also, Krugs can be used by any user and not just root.
 
 Krugs only works Unix-like systems.
 
+    niklas@sunbird:~$ krugs status
+    Krugs: [test]: stopped
+    niklas@sunbird:~$ krugs start test
+    Krugs: [test]: running: /home/niklas/Desktop/my-daemon.sh
+    Krugs: [test]: started (PID: 4717)
+    niklas@sunbird:~$ krugs status
+    Krugs: [test]: started
+    niklas@sunbird:~$ krugs stop test
+    Krugs: [test]: stopping... stopped
+    niklas@sunbird:~$ krugs status
+    Krugs: [test]: stopped
+
 __Configuration__
 
-Krugs is configured with a Python script. This script is imported when
-Krugs is loaded. Take a look at the [`krugs_config.py`](krugs_config.py) file
-for the configuration template.
+Krugs is configured with a Python script in your home directory called
+`krugs_config.py`. Below you can find an example of what that file could
+look like. It is fairly long due to the commentary. For this configuration
+script, the following functions are already available and need not be
+imported manually:
 
-To configure a daemon, you write it like this:
+* `join()`, `split()` and `expanduser()` from `os.path`
+* `register_daemon()` from `krugs`
 
 ```python
-Daemon(
+# The path of the folder that contains the PID files and standard error
+# and output files for relative paths.
+root_dir = expanduser('~/.krugs/run')
+
+# The maximum number of seconds to wait after SIGTERM to send SIGKILL.
+kill_timeout = 10
+
+# Declare daemons using the #register_daemon() function. Example:
+register_daemon(
+  # The name of the daemon that you will use to address it on
+  # the command-line.
   name = 'test',
-  bin = expath('~/Desktop/test.sh'),
-  args = ['42'],
+
+  # The name or full path of the program to execute.
+  prog = expanduser('~/Desktop/my-daemon.sh'),
+
+  # Additional command-line arguments for the daemon.
+  args = [],
+
+  # The directory to change to for executing the daemon program.
+  # The path is passed through #expanduser(), thus `~` will be
+  # the home directory of the user that the daemon is run under
+  # (important if you changed the user parameter below).
+  # Passing None will make the path default to the executing users
+  # home directory as well.
+  cwd = '~',
+
+  # The username to log-in to when starting the daemon. Sets HOME,
+  # but does not using shell login. The user that starts the
+  # daemon with Krugs must be allowed to switch to that user.
   user = None,
+
+  # The user group to switch to.
   group = None,
+
+  # The file for stdin. Defaults to /dev/null.
+  stdin = None,
+
+  # The output file for stdout. Defaults to $root_dir/$name.out .
+  # Explicitly use /dev/null if you want it.
+  stdout = None,
+
+  # The output file for stderr. Defaults to the exact same file
+  # as stdout when set to None.
+  stderr = None,
+
+  # The filename where the PID is saved to. Defaults to
+  # $root_dir/$name.pid .
+  pidfile = None,
 )
 ```
 
 __Todo__
 
-* [ ] Code cleanup
 * [ ] Support for daemon dependencies
 
 __Requirements__
--- krugs.py
+++ krugs.py
@@ -18,52 +18,73 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+__author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
+__version__ = '1.0.0'
+
 import argparse
 import errno
 import os
 import pwd, grp
-import time
+import runpy
 import shlex
 import signal
 import subprocess
 import sys
+import time
+import types
 
-# Imported later to give the config file access to the Krugs API
+# The configuration module object that is imported later.
 config = None
+
+# A dictionary of all daemons registered with #register_daemon().
+daemons = {}
 
 
 def abspath(path):
+  ''' Make *path* absolute if it not already is. Relative paths
+  are assumed relative to the ``config.root_dir`` configuration
+  parameter.
+
+  .. note:: This function can not be used before the :data:`config`
+    is loaded.
+  '''
+
   if not os.path.isabs(path):
     return os.path.abspath(os.path.join(config.root_dir, path))
   return path
 
 
+def makedirs(path):
+  ''' Create the directory *path* if it does not already exist. '''
+
+  if not os.path.exists(path):
+    os.makedirs(path)
+
+
+def process_exists(pid):
+  ''' Checks if the process represented by *pid* exists and returns
+  True in that case, otherwise False. '''
+
+  if pid == 0:
+    return False
+  try:
+    os.kill(pid, 0)
+  except OSError:
+    return False
+  return True
+
+
 class Daemon(object):
-  ''' Configuration for a daemon process.
+  ''' Configuration for a daemon process. See ``krugs_config.py``
+  for an explanation of the parameters. '''
 
-  :param name: The name of the daemon. This name is used when invoking
-    Krugs to address a specific daemon.
-  :param bin: Path to the daemon executable/script.
-  :param args: A list of additional arguments for the daemon process.
-  :param stdout: Filename to pipe the standard output to. If None,
-    the file is piped into ``dev/null``.
-  :param stderr: Filename to pipe the standard output to. If None,
-    the file is piped into the same file/device as *stdout*.
-  :param user: The name of the user to execute the daemon as.
-  :param group: The name of the group to execute the daemon as.
-  :param cwd: The working directory for the daemon process.
-    Defaults to the HOME directory of the user that executes
-    the process.
-  '''
+  Status_Started = 'started'
+  Status_Stopped = 'stopped'
 
-  objects = {}
+  def __init__(
+      self, name, prog, args=(), cwd=None, user=None, group=None,
+      stdin=None, stdout=None, stderr=None, pidfile=None):
 
-  def __init__(self, name, bin, args=(), pidfile=None,
-      stdin='/dev/null', stdout=None, stderr=None,
-      user=None, group=None, cwd=None):
-
-    if name in Daemon.objects:
-      raise ValueError('daemon name {!r} already reserved'.format(name))
     if cwd and not os.path.isdir(cwd):
       raise ValueError('directory {!r} does not exist'.format(cwd))
 
@@ -72,61 +93,98 @@ class Daemon(object):
     if stdout is None:
       stdout = abspath(name + '.out')
 
+    self._log_newline = True
     self.name = name
-    self.bin = bin
+    self.prog = prog
     self.args = list(args)
-    self.pidfile = pidfile
-    self.stdin = stdin
-    self.stdout = stdout
-    self.stderr = stderr
+    self.cwd = cwd
     self.user = user
     self.group = group
-    self.cwd = cwd
-    Daemon.objects[name] = self
+    self.stdin = stdin or '/dev/null'
+    self.stdout = stdout
+    self.stderr = stderr
+    self.pidfile = pidfile
 
+  def __repr__(self):
+    return '<Daemon {!r}: {}>'.format(self.name, self.status)
+
+  @property
   def pid(self):
+    ''' Returns the PID of the daemon. It is read from the file
+    as referenced by the :attr:`pidfile` attribute. If the file
+    contains an invalid PID or is empty or if the file does not
+    exist, 0 is returned. '''
+
     try:
       with open(self.pidfile, 'r') as fp:
-        value = fp.read().strip()
-        try:
-          return int(value)
-        except ValueError:
-          return 0
+        pid_str = fp.readline()
     except OSError as exc:
       if exc.errno != errno.ENOENT:
         raise
-      return 0
-
-  def start(self):
-    ''' Start the Daemon if it is not already running. '''
-
-    if self.status():
-      print('[{}]: already running')
-      return True
-
-    print('[{}]: starting ...'.format(self.name))
-
-    home, uid, gid = None, None, None
-    if self.user:
-      home, uid, gid = get_user_info(self.user)
-    if self.group:
-      gid = get_group_id(self.group)
-
-    # Fork so we can update detach, etc.
-    pid = os.fork()
-    if pid > 0:
-      # Wait until the child process started the actual daemon.
-      if os.waitpid(pid, 0)[1] == 0:
-        print('[{}]: started (PID: {})'.format(self.name, self.pid()))
-      else:
-        print('[{}]: could not be started'.format(self.name))
-      return True
-
-    # Open the PID file.
-    makedirs(os.path.dirname(self.pidfile))
-    pidf = open(self.pidfile, 'w')
+      pid_str = ''
 
     try:
+      return int(pid_str)
+    except ValueError:
+      return 0
+
+  @property
+  def status(self):
+    ''' Reads the PID and checks if the process under that PID
+    exists. Returns :data:`Status_Stopped` or :data:`Status_Started`. '''
+
+    if process_exists(self.pid):
+      return self.Status_Started
+    else:
+      return self.Status_Stopped
+
+  def log(self, *message, **kwargs):
+    ''' Prints a message with the name of the daemon as its prefix. '''
+
+    if self._log_newline:
+      print('Krugs: [{}]:'.format(self.name), *message, **kwargs)
+    else:
+      print(*message, **kwargs)
+    self._log_newline = '\n' in kwargs.get('end', '\n')
+    kwargs.get('file', sys.stdout).flush()
+
+  def start(self):
+    ''' Start the daemon if it is not already running. Returns True
+    if the daemon is already running or could be started, False if
+    it could not be started. '''
+
+    if self.status == self.Status_Started:
+      self.log('daemon already started')
+      return True
+
+    # Get the user and group information if any.
+    home, uid, gid = None, None, None
+    if self.user:
+      record = pwd.getpwnam(self.user)
+      home, uid, gid = record.pw_dir, record.pw_uid, record.pw_gid
+    if self.group:
+      record = grp.getgrnam(self.group)
+      gid = record.gr_gid
+
+    # Fork so we can update detach, etc. The parent waits until
+    # the child exits to figure if the process could be started.
+    pid = os.fork()
+    if pid > 0:
+      if os.waitpid(pid, 0)[1] == 0:
+        self.log('started (PID: {})'.format(self.pid))
+      else:
+        self.log('could not be started, check the output file eventually')
+      return True
+
+    # Make sure the directory of the PID and output files exist.
+    makedirs(os.path.dirname(self.pidfile))
+    makedirs(os.path.dirname(self.stdin))
+    makedirs(os.path.dirname(self.stdout))
+    makedirs(os.path.dirname(self.stderr or self.stdout))
+
+    pidf = open(self.pidfile, 'w')
+    try:
+      # Detach the process and set the user and group IDs if applicable.
       os.setsid()
       if uid is not None:
         try:
@@ -134,7 +192,7 @@ class Daemon(object):
         except OSError as exc:
           if exc.errno != errno.EPERM:
             raise
-          print('[{}]: not permitted to change to user {!r}'.format(self.name, self.user))
+          self.log('not permitted to change to user {!r}'.format(self.user))
           sys.exit(errno.EPERM)
       if gid is not None:
         try:
@@ -142,129 +200,146 @@ class Daemon(object):
         except OSError as exc:
           if exc.errno != errno.EPERM:
             raise
-          print('[{}]: not permitted to change to group {!r}'.format(self.name, self.group))
+          self.log('not permitted to change to group {!r}'.format(self.group))
           sys.exit(errno.EPERM)
+
+      # Update the HOME environment variable and switch the working
+      # directory for the daemon process.
       if home:
         os.environ['HOME'] = home
-
       if self.cwd:
-        os.chdir(self.cwd)
+        os.chdir(os.path.expanduser(self.cwd))
       else:
         os.chdir(os.environ['HOME'])
 
-      # Generate the comamnd to execute.
-      command = [self.bin] + self.args
-
-      # Redirect the standard in, out and error.
+      # Open the input file and output files for the daemon.
       si = open(self.stdin, 'r')
       so = open(self.stdout, 'a+')
-      if self.stderr:
-        se = open(self.stderr, 'a+')
-      else:
-        se = so
+      se = open(self.stderr, 'a+') if self.stderr else so
 
-      # Print the command before updating the in/out/err file handles.
-      print('[{}]: $'.format(self.name), ' '.join(map(shlex.quote, command)))
+      # Print the command before updating the in/out/err file handles
+      # so the caller of Krugs can still read it.
+      command = [self.prog] + self.args
+      self.log('running:', ' '.join(map(shlex.quote, command)))
+
+      # Replace the standard file handles and execute the daemon process.
       os.dup2(si.fileno(), sys.stdin.fileno())
       os.dup2(so.fileno(), sys.stdout.fileno())
       os.dup2(se.fileno(), sys.stderr.fileno())
-
-      # Execute the daemon.
       process = subprocess.Popen(command)
       pidf.write(str(process.pid))
-
       sys.exit(0)
     finally:
       pidf.close()
 
-  def status(self, pid=None, do_print=False):
-    ''' Returns True if the process is running, otherwise false. '''
-
-    if pid is None:
-      pid = self.pid()
-    if pid == 0:
-      if do_print:
-        print('[{}]: stopped'.format(self.name))
-      return False
-    if not pid_exists(pid):
-      if do_print:
-        print('[{}]: stopped'.format(self.name))
-      return False
-    else:
-      if do_print:
-        print('[{}]: running'.format(self.name))
-      return True
-
   def stop(self):
-    pid = self.pid()
+    ''' Stop the daemon if it is running. Sends :data:`signal.SIGTERM`
+    first, then waits at maximum ``config.kill_timeout`` seconds and
+    sends ``signal.SIGKILL`` if the process hasn't terminated by then. '''
+
+    pid = self.pid
     try:
       os.kill(pid, signal.SIGTERM)
     except OSError:
-      print('[{}]: not running'.format(self.name))
+      self.log('not started')
     else:
-      print('[{}]: SIGTERM sent'.format(self.name))
+      self.log('stopping...', end=' ')
       tstart = time.time()
-      while time.time() - tstart < config.kill_timeout and pid_exists(pid):
+      while time.time() - tstart < config.kill_timeout and process_exists(pid):
         time.sleep(0.5)
-      if pid_exists(pid):
+      if process_exists(pid):
+        self.log('not stopped.', end=' ')
         try:
           os.kill(pid, signal.SIGKILL)
         except OSError:
-          pass
+          self.log('stopped')
         else:
-          print('[{}]: SIGKILL sent'.format(self.name))
-      print('[{}]: stopped'.format(self.name))
+          self.log('SIGKILL')
+      else:
+        self.log('stopped')
 
 
-def get_user_info(user_name):
-  record = pwd.getpwnam(user_name)
-  return record.pw_dir, record.pw_uid, record.pw_gid
+def register_daemon(**kwargs):
+  ''' Register a daemon to Krugs. The arguments are the same as for
+  the :class:`Daemon` class, though only keyword-arguments are accepted. '''
+
+  daemon = Daemon(**kwargs)
+  if daemon.name in daemons:
+    raise ValueError('daemon name {!r} already used'.format(daemon))
+  daemons[daemon.name] = daemon
 
 
-def get_group_id(group_name):
-  record = grp.getgrnam(group_name)
-  return record.gr_gid
+def load_config(filename):
+  ''' Load the Krugs configuration from *filename*. This effectively
+  executes *filename* and returns a Python module object. '''
 
+  module = types.ModuleType('krugs_config')
+  module.__file__ = filename
+  module.join = os.path.join
+  module.split = os.path.split
+  module.expanduser = os.path.expanduser
+  module.register_daemon = register_daemon
 
-def makedirs(path):
-  if not os.path.exists(path):
-    os.makedirs(path)
+  global config
+  config = module
 
-
-def pid_exists(pid):
   try:
-    os.kill(pid, 0)
-  except OSError:
-    return False
-  return True
+    with open(filename) as fp:
+      exec(fp.read(), vars(module))
+  except:
+    config = None
+    raise
 
 
 def main():
   parser = argparse.ArgumentParser(prog='krugs', description='A simple daemon manager')
-  parser.add_argument('daemon', choices=Daemon.objects.keys())
-  parser.add_argument('command', choices=['start', 'stop', 'restart', 'status'])
+  parser.add_argument('command', choices=['start', 'stop', 'restart', 'status', 'version', 'list'])
+  parser.add_argument('daemons', nargs='*', default=[])
   args = parser.parse_args()
 
-  daemon = Daemon.objects[args.daemon]
-  if args.command == 'start':
-    if daemon.start():
-      sys.exit(0)
-    sys.exit(1)
-  elif args.command == 'stop':
-    if daemon.stop():
-      sys.exit(0)
-    sys.exit(1)
+  if args.command == 'version':
+    print('Krugs v{}'.format(__version__))
+    return 0
+
+  # Load the Krugs configuration file.
+  config_file = os.path.expanduser('~/krugs_config.py')
+  if not os.path.isfile(config_file):
+    parser.error('~/krugs_config.py does not exist')
+  load_config(config_file)
+
+  try:
+    target_daemons = [daemons[x] for x in args.daemons]
+  except KeyError as exc:
+    parser.error('unknown daemon {!r}'.format(str(exc)))
+
+  if args.command == 'status':
+    for daemon in (target_daemons or daemons.values()):
+      daemon.log(daemon.status)
+    return 0
+  elif args.command == 'list':
+    for daemon in daemons.keys():
+      print(daemon)
+    return 0
+  elif not args.daemons:
+    parser.error('need at least one argument for "daemons"')
+
+  if args.command == 'stop':
+    for daemon in target_daemons:
+      if not daemon.stop():
+        return 1
+    return 0
+  elif args.command == 'start':
+    for daemon in target_daemons:
+      if not daemon.start():
+        return 1
+    return 0
   elif args.command == 'restart':
-    daemon.stop()
-    if daemon.start():
-      sys.exit(0)
-    sys.exit(1)
-  elif args.command == 'status':
-    if daemon.status(do_print=True):
-      sys.exit(0)
-    sys.exit(1)
-  else:
-    assert False
+    for daemon in target_daemons:
+      if not daemon.stop():
+        return 1
+    for daemon in target_daemons:
+      if not daemon.start():
+        return 1
+    return 0
 
-
-import krugs_config as config
+  parser.error('unknown command {!r}'.format(args.command))
